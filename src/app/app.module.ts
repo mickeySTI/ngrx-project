a1b7c9b99dddd71b87009edaa1d7905a4ef80603
MGarcia1120 <imickey216@gmail.com>
2021-12-08 03:31:05 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
+import { AuthModule } from './auth/auth.module';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        CommonModule,
+        AuthModule,
+        AppRoutingModule,
+        StoreModule.forRoot(reducers, {
+            metaReducers,
+        }),
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
